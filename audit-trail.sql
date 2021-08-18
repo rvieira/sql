0a968a50bb15b96227b3e0a41c3d3402c9440501
Ricardo Vieira <ricardo@rvieira.co.uk>
2021-08-18 11:31:02 +0200

    Update audit-trail.sql
    generic username

--- audit-trail.sql
+++ audit-trail.sql
@@ -1,4 +1,4 @@
 select os_username,userhost,owner,action_name,extended_timestamp
 from dba_audit_trail
-where timestamp > to_date('03-JUL-19 22.00.00','dd-mon-yy hh24.mi.ss') and current_user='SOPHIS'
+where timestamp > to_date('03-JUL-19 22.00.00','dd-mon-yy hh24.mi.ss') and current_user='<username>'
 /
