e78a811a2b6029746ddc1fe0949d6543b5c7a087
rvieira <ricardo@rvieira.co.uk>
2021-08-27 11:53:30 +0200

    new file:   asm/ASM17.sql 	new file:   asm/asm-file-details.sql 	new file:   asm/asm-list-disks.sql 	new file:   asm/asm-space.sql 	new file:   asm/cr8-asm_usage_hist.sql

---
+++ asm/asm-space.sql
@@ -0,0 +1,10 @@
+SELECT 
+    name, 
+    type, 
+    total_mb/1024/1024 "Total TB", 
+    free_mb/1024/1024 "Free TB", 
+    required_mirror_free_mb,
+    usable_file_mb,
+    free_mb/total_mb*100 as percentage
+FROM 
+    V$ASM_DISKGROUP;
