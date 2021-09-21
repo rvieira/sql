3c2ea9d2a4d20382d5226df8c500b14a3611d3b2
rvieira <ricardo@rvieira.co.uk>
2021-09-21 13:22:49 +0200

    Added header to audit-trail.sql

--- audit-trail.sql
+++ audit-trail.sql
@@ -1,3 +1,10 @@
+----------------------------------------------------------------------------------------------------
+-- Author: Ricardo Vieira
+-- Created: 2018
+-- Updates:
+--    21.09.2021 - Added this header
+-- Description:
+----------------------------------------------------------------------------------------------------
 select os_username,userhost,owner,action_name,extended_timestamp
 from dba_audit_trail
 where timestamp > to_date('03-JUL-19 22.00.00','dd-mon-yy hh24.mi.ss') and current_user='<username>'
