173b57a0870acbe685473e84a54293c0582971e5
rvieira <ricardo@rvieira.co.uk>
2021-09-24 20:53:10 +0200

    new file:   cache-hit-ratio.sql 	modified:   directories.sql 	modified:   extract-pwd-hashes.sql 	new file:   free-space.sql 	new file:   longops.sql 	new file:   obj-lock.sql 	new file:   session-waits.sql 	new file:   sessions-by-machine.sql 	new file:   sessions.sql 	new file:   show-space.sql Scripts from Tim Hall and Tom Kyte

--- directories.sql
+++ directories.sql
@@ -1,1 +1,9 @@
+-- ------------------------------------------------------------------------------------------------------------
+-- File Name    : https://github.com/rvieira/sql/blob/1e9e55a106eea3198652b68aa2c4ee2a4010ff04/directories.sql
+-- Author       : Ricardo Vieira
+-- Description  : Displays the directories defined in the database.
+-- Requirements : Access to DBA views.
+-- Call Syntax  : @directories
+-- Last Modified: 
+-- ------------------------------------------------------------------------------------------------------------
 select * from dba_directories;
--- extract-pwd-hashes.sql
+++ extract-pwd-hashes.sql
@@ -1,3 +1,11 @@
+-- --------------------------------------------------------------------------------------------------------------------
+-- File Name    : https://github.com/rvieira/sql/blob/1e9e55a106eea3198652b68aa2c4ee2a4010ff04/extract-pwd-hashes.sql
+-- Author       : Ricardo Vieira
+-- Description  : Displays the directories defined in the database.
+-- Requirements : 
+-- Call Syntax  : @extract-pwd-hashes
+-- Last Modified: 
+-- --------------------------------------------------------------------------------------------------------------------
 with t as
   ( select dbms_metadata.get_ddl('USER',username) ddl from dba_users where username like '%&schema%')
   select replace(substr(ddl,1,instr(ddl,'DEFAULT')-1),'CREATE','ALTER')||';'
