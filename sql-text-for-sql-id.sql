995414112ea3ba992fd9a4a35b0f881be1f9c996
rvieira <ricardo@rvieira.co.uk>
2021-09-30 15:49:42 +0200

    modified:   sql-text-for-sql-id.sql

--- sql-text-for-sql-id.sql
+++ sql-text-for-sql-id.sql
@@ -1,7 +1,7 @@
 ------------------------------
 -- Anjum, this one is for you
 ------------------------------
-select *
+select sql_text
 from v$sqltext
 where SQL_ID = '&sqlid' 
 order by piece asc;
