8d06636bb373f5587bf234a8cf0a16c3b3bbdbba
rvieira <ricardo@rvieira.co.uk>
2021-09-30 15:37:22 +0200

    modified:   build-kill-killed.sql 	new file:   sql_text_by_sid.sql

--- build-kill-killed.sql
+++ build-kill-killed.sql
@@ -1,4 +1,3 @@
 select * from v$session where status = 'KILLED';
 select count(*) from v$session;
 select 'alter system kill session '''||sid||','||serial#||''';' from v$session where status = 'KILLED';
-
