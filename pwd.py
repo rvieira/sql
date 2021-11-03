2185c927f2bcccffe1504e3b6ace3dcbc618ddc1
rvieira <ricardo@rvieira.co.uk>
2021-11-03 15:45:55 +0100

    Added compliance checks to pwd.py 	modified:   pwd.py

--- pwd.py
+++ pwd.py
@@ -8,7 +8,25 @@
 import secrets
 import string
 
+def compliant_pwd(candidate_pwd):
+    check_upper = False
+    check_lower = False
+    check_special_char = False
+    
+#    print('try --> '+candidate_pwd)
+    for letter in candidate_pwd:
+        if letter.isupper():
+            check_upper = True
+        if letter.islower():
+            check_lower = True
+        if letter in ('._+-'):
+            check_special_char = True
+    return check_lower and check_upper and check_special_char
+        
 password_length=20
-alphabet = string.ascii_letters + string.digits + '._-+'
-password = ''.join(secrets.choice(alphabet) for i in range(password_length))  # for a 20-character password
+special_characters = '._+-'
+password = ''
+alphabet = string.ascii_letters + string.digits + special_characters
+while not compliant_pwd(password):
+    password = ''.join(secrets.choice(alphabet) for i in range(password_length))
 print(password)
